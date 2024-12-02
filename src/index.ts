2de2457ecdfb39ace2f258af57d9b0c6d9887a8c
Takumi Takahashi <takumiiinn@gmail.com>
2024-12-03 02:22:08 +0900

    initial implemented

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { ChatOllama } from "@langchain/community/chat_models/ollama";
+
+const chatModel = new ChatOllama({
+	baseUrl: "http://localhost:11434",
+	model: "qwen2.5-coder:32b",
+});
+
+const result = await chatModel.invoke("what is LangSmith? speak japanese");
+
+console.log(result);
